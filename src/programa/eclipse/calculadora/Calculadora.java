ee95cd97ee1e3b75fbd525eb3996e99724c64209
LucasFe97 <lucasferreirasd82@gmail.com>
2023-04-17 19:22:55 -0300

    Update

--- src/programa/eclipse/calculadora/Calculadora.java
+++ src/programa/eclipse/calculadora/Calculadora.java
@@ -7,17 +7,17 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int a = 0;
-		int b = 0;
+		double a = 0;
+		double b = 0;
 		System.out.println("Digite seu primeiro valor: ");
 		a = scan.nextInt();
 		System.out.println("Digite seu segundo valor: ");
 		b = scan.nextInt();
 		
-		int soma = soma(a,b);
-		int multiplicacao = multiplicacao(a,b);
-		int subtracao = subtracao(a,b);
-		int divisao = divisao(a,b);
+		double soma = soma(a,b);
+		double multiplicacao = multiplicacao(a,b);
+		double subtracao = subtracao(a,b);
+		double divisao = divisao(a,b);
 		
 		System.out.println(soma);
 		System.out.println(multiplicacao);
@@ -27,19 +27,19 @@ public class Calculadora {
 		
 	}
 
-		public static int soma (int a, int b) {
+		public static double soma (double a, double b) {
 			return a + b;
 		}		
 		
-		public static int multiplicacao (int a, int b) {
+		public static double multiplicacao (double a, double b) {
 			return a * b;
 		}		
 		
-		public static int subtracao (int a, int b) {
+		public static double subtracao (double a, double b) {
 			return a - b;
 		}		
 		
-		public static int divisao (int a, int b) {
+		public static double divisao (double a, double b) {
 			return a / b;
 		}		
 
